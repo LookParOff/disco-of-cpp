6fd6a0beded0d6166a7f8985883c97574077c8a3
PaRoNoIk <20956901+PaRoNoIk@users.noreply.github.com>
2020-02-04 15:15:54 +0300

    task7 1,2

---
+++ task7/task7_nearest/task7_nearest.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+float nearest_el(std::vector<int>::const_iterator iterBeg, std::vector<int>::const_iterator iterEnd, float R)
+{
+	float near = INFINITY;
+	float min = abs(*iterBeg - R);
+	while (iterBeg != iterEnd)
+	{
+		if (abs(*iterBeg - R) < min)
+		{
+			near = *iterBeg;
+		}
+		++iterBeg;
+	}
+	return near;
+}
+
+int main()
+{
+	std::cout << "near" << std::endl;
+	std::vector<int> arr = { 1,2,3,4,7 };
+	std::vector<int>::const_iterator iterBeg;
+	iterBeg = arr.cbegin();
+
+	std::vector<int>::const_iterator iterEnd;
+	iterEnd = arr.cend();
+
+	std::cout << nearest_el(iterBeg, iterEnd, 6) << std::endl;
+}
+
